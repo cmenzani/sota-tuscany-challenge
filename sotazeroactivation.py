1850ad73a01a338593a05ba15af9a73f57e3eb54
cmenzani <cmenzani@vivido.it>
2024-04-16 11:32:16 +0200

    risultato formattato

--- sotazeroactivation.py
+++ sotazeroactivation.py
@@ -10,11 +10,15 @@ Cime mai attivate Valide: 115
 che ad un controllo sia su SOTL.AS che SOTAMPAS.ORG risultano corretti
 """
 import json
+import datetime
 
 # Apri il file tuscanysummit.json in modalità lettura
 with open('tuscanysummit.json', 'r') as file:
     # Carica il contenuto JSON dal file
     data = json.load(file)
+
+    # Data e ora corrente
+    current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     # Crea una lista per gli elementi JSON con "ActivationCount" uguale a "0"
     never_activated = [item for item in data if item.get("ActivationCount") == "0"]
@@ -27,6 +31,7 @@ with open('tuscanysummit.json', 'r') as file:
 
     # Calcola i totali
     total_elements = {
+        "Data elaborazione": current_datetime,
         "Totale cime SOTA in Toscana": len(data),
         "Cime mai attivate": len(never_activated),
         "Totale cime Inactive": len(inactive_summits),
@@ -34,8 +39,16 @@ with open('tuscanysummit.json', 'r') as file:
         "Cime mai attivate Valide": len(valid_never_activated)  # Correttamente filtrate
     }
 
-# Stampa a console i totali per facile riferimento
-print(f"Totale cime SOTA in Toscana: {total_elements['Totale cime SOTA in Toscana']}")
-print(f"Totale cime Non Valide/Inactive: {total_elements['Totale cime Inactive']}")
-print(f"Totale cime Valide: {total_elements['Totale cime Valide']}")
-print(f"Cime mai attivate Valide: {total_elements['Cime mai attivate Valide']}")  # Stampato il valore corretto
+    # Scrive tutto in un unico file JSON, includendo il timestamp
+    output_data = {"Timestamp": current_datetime, "Data": total_elements}
+    with open('tuscanyzeroactivation.json', 'w') as outfile:
+        json.dump(output_data, outfile, indent=4)
+
+    # Stampa a console con timestamp
+    print("======================================")
+    print(f"Data elaborazione: {current_datetime}")
+    print("======================================")
+    print(f"Totale cime SOTA in Toscana: {total_elements['Totale cime SOTA in Toscana']}")
+    print(f"Totale cime Non Valide/Inactive: {total_elements['Totale cime Inactive']}")
+    print(f"Totale cime Valide: {total_elements['Totale cime Valide']}")
+    print(f"Cime mai attivate Valide: {total_elements['Cime mai attivate Valide']}")  # Stampato il valore corretto
